c572bee3876093d9a85005d6dcbb79ab307bae8d
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-21 23:52:06 -0700

    created database and detailed user table schema

---
+++ habit-tracker-express-api/habit-tracker-schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id              SERIAL PRIMARY KEY,
+    first_name      TEXT NOT NULL,
+    last_name       TEXT NOT NULL,
+    email           TEXT NOT NULL UNIQUE CHECK (POSITION('@' IN EMAIL) > 1),
+    username        TEXT NOT NULL UNIQUE,
+    password        TEXT NOT NULL,
+    created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at      TIMESTAMP NOT NULL DEFAULT NOW()
+);
