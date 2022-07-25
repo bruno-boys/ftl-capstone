e6fbed41e465a3ac3432742a25d0308b9a2fcff4
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-25 14:10:31 -0700

    created apiClient in frontend to allow for persistent login

---
+++ habit-tracker-ui/src/services/apiClient.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+class ApiClient {
+
+    constructor(remoteHostUrl) {
+        this.remoteHostUrl = remoteHostUrl
+        this.token = localStorage.getItem("life_tracker_token") || null
+        this.tokenName = "life_tracker_token"
+    }
+
+    setToken(token) {
+        this.token = token
+        localStorage.setItem(this.tokenName, token)
+    }
+
+    async request({ endpoint, method = 'GET', data = {} }) {
+
+        const url = `${this.remoteHostUrl}/${endpoint}`
+
+        const headers = {
+            "Content-Type": "application/json"
+        }
+
+        if (this.token) {
+            headers["Authorization"] = `Bearer ${this.token}`
+        }
+
+        try {
+            const res = await axios({ url, method, data, headers })
+            return { data: res.data, error: null }
+        }
+        catch(error) {
+            console.error({ errorResponse: error.response })
+            const message = error?.response
+            return { data: null, error: message || String(error) }
+        }
+    }
+
+    async fetchUserFromToken() {
+        return await this.request({ endpoint: `auth/me`, method: `GET` })
+    }
+
+    async loginUser(credentials) {
+        return await this.request({ endpoint: `auth/login`, method: `POST`, data: credentials })
+    }
+
+    async registerUser(credentials) {
+        return await this.request({ endpoint: `auth/register`, method: `POST`, data: credentials })
+    }
+
+}
+
+export default new ApiClient('http://localhost:3001')
