382201ed89ffbd3b1013f33bc3efe5366b0dc6da
Abdul Rauf Abdul Karim <abdul.karim@abdulka-ltmat6w.internal.salesforce.com>
2022-07-25 11:39:49 -0700

    Added the front end view of the login page. User is able to type their username and password

--- habit-tracker-ui/src/components/Login/Login.jsx
+++ habit-tracker-ui/src/components/Login/Login.jsx
@@ -1,9 +1,34 @@
 import "./Login.css";
 
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="Login">
+      <div className="card">
+        <h2> Login </h2>
+        <div className="form">
+        <div className="input-field">
+          <label htmlFor="username">Username</label>
+          <input type='text' name="username" placeholder="Username"/>
+        </div>
+
+        <div className="input-field">
+          <label htmlFor="password"> Password</label>
+          <input type='email' name = 'email' placeholder = 'Email' />
+
+        </div>
+
+        <button className="btn"> Login </button>
+
+      </div>
+
+      <div className="footer">
+          <p>
+            Don't have an account? Sign up
+          </p>
+        </div>
+        </div>
+    </div>
+  );
 }
