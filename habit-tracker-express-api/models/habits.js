fe7ce9d6672852ded830d92ee9269de218631fb5
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-25 15:27:10 -0700

    Created skeleton for Habits model and created initial Habits routes

---
+++ habit-tracker-express-api/models/habits.js
@@ -0,0 +1,15 @@
+const db = require('../db')
+
+class Habits {
+
+    static async fetchHabitList(user) {
+        // retrieves all user habits
+    }
+
+    static async createHabit(user) {
+        //allows user to create habits
+    }
+    
+}
+
+module.exports = Habits;
