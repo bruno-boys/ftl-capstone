5b080656b214ae87e2eaf10688c9ac3ae1da25ff
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-25 16:03:16 -0700

    Created fetch habit method for habit page
    wrote fetchHabit function in Habits model and created apiClient GET request

--- habit-tracker-express-api/models/habits.js
+++ habit-tracker-express-api/models/habits.js
@@ -3,13 +3,22 @@ const db = require('../db')
 class Habits {
 
     static async fetchHabitList(user) {
-        // retrieves all user habits
+        // retrieves all user habits using the user's email to filter the tables
+        const results = await db.query(
+            `
+            SELECT * FROM habits
+            WHERE users_id = (SELECT id FROM users WHERE email = $1)
+            `, [user.email]
+        );
+
+        return results.rows
     }
+
 
     static async createHabit(user) {
         //allows user to create habits
     }
-    
+
 }
 
 module.exports = Habits;
