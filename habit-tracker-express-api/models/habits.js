7ee3ca3688be3aa07da3d30ca9f5aea53893e17a
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-26 11:57:24 -0700

    Completed backend for habit details
    detailed out the habit/:id route which returns the habit based on the id given and the user.

--- habit-tracker-express-api/models/habits.js
+++ habit-tracker-express-api/models/habits.js
@@ -7,18 +7,32 @@ class Habits {
         const results = await db.query(
             `
             SELECT * FROM habits
-            WHERE users_id = (SELECT id FROM users WHERE email = $1)
+            WHERE users_id = (SELECT id FROM users WHERE email = $1);
             `, [user.email]
         );
 
         return results.rows
     }
 
+    static async fetchHabitById(user, habitId) {
+        //allows for user to fetch a single habit by its id
+        const results = await db.query(
+            `
+            SELECT * FROM habits
+            WHERE (users_id = (SELECT id FROM users WHERE email = $1))
+                AND (id = (SELECT id FROM habits WHERE id = $2));
+            `, [user.email, habitId]
+        );
+        return results.rows[0]
+    }
+
     static async createHabit(user, habitForm) {
         //allows user to create habits
         await db.query(
-            ` INSERT INTO habits (users_id, habit_name, frequency, period, start_date, end_date)
-            VALUES ((select id from users where email = $1), $2, $3, $4, $5, $6);`, 
+            ` 
+            INSERT INTO habits (users_id, habit_name, frequency, period, start_date, end_date)
+            VALUES ((select id from users where email = $1), $2, $3, $4, $5, $6);
+            `, 
             [user.email, habitForm.habitName, habitForm.frequency, habitForm.period, habitForm.startDate, habitForm.endDate]
         )
     }
