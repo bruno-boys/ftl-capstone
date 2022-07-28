4860c0b2e4fac64caf03644858ce9c82c3234a10
Yaw Kessey-Ankomah <yawkesseyankomah@gmail.com>
2022-07-28 11:11:27 -0700

    Added models and routes to delete a habit

--- habit-tracker-express-api/routes/habits.js
+++ habit-tracker-express-api/routes/habits.js
@@ -16,23 +16,33 @@ router.get("/", requireAuthenticatedUser, async (req, res, next) => {
   }
 });
 
-router.get('/:id', requireAuthenticatedUser, async (req,res,next) => {
-  try{
-    const user = res.locals.user
-    const habitId = parseInt(req.params.id)
-    const habit = await Habits.fetchHabitById(user, habitId)
-    res.status(200).json(habit)
-  }
-  catch(error) {
+router.get("/:id", requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    const user = res.locals.user;
+    const habitId = parseInt(req.params.id);
+    const habit = await Habits.fetchHabitById(user, habitId);
+    res.status(200).json(habit);
+  } catch (error) {
     next(error);
   }
-})
+});
+
+router.delete("/:id", requireAuthenticatedUser, async (req, res, next) => {
+  try {
+    const user = res.locals.user;
+    const habitId = parseInt(req.params.id);
+    await Habits.deleteHabit(user, habitId);
+    res.status(200).json({ message: "Habit deleted" });
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.post("/create", requireAuthenticatedUser, async (req, res, next) => {
   try {
     const user = res.locals.user;
     await Habits.createHabit(user, req.body);
-    res.status(200).json({status: "Success!"})
+    res.status(200).json({ status: "Success!" });
   } catch (error) {
     next(error);
   }
