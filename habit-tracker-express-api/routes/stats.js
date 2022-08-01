38ba6d23e0c237679d91610938f15b4f4663fc59
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-08-01 13:23:37 -0700

    added method skeletons to stats model file

--- habit-tracker-express-api/routes/stats.js
+++ habit-tracker-express-api/routes/stats.js
@@ -4,8 +4,18 @@ const { requireAuthenticatedUser } = require("../middleware/security");
 
 const router = express.Router();
 
-router.get('/', async(req,res,next) => {
+router.get('/', requireAuthenticatedUser, async(req,res,next) => {
     res.status(200).json("main stats route works")
 })
+
+router.get('/habit-streaks', requireAuthenticatedUser, async(req,res,next) => {
+    try {
+        res.status(200).json("main stats route works")
+    }
+    catch(error) {
+        next(error)
+    }
+})
+
 
 module.exports = router;
