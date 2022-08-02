52c6c3f12d9d4da05e11a7872ee6bc2112b25fbc
Yaw Kessey-Ankomah <yawkesseyankomah@gmail.com>
2022-08-01 17:43:05 -0700

    Added config to get password reset emails

--- habit-tracker-express-api/config.js
+++ habit-tracker-express-api/config.js
@@ -1,39 +1,55 @@
-require('dotenv').config()
-require('colors')
+require("dotenv").config();
+require("colors");
 
-const PORT =  process.env.PORT ? Number(process.env.PORT) : 3001
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
 
-const SECRET_KEY = process.env.SECRET_KEY || 'secret_dev'
+const APPLICATION_NAME = "Habit Tracker";
+const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
 
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
+const SECRET_KEY = process.env.SECRET_KEY || "secret_dev";
+
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 // Should be a terinary operator to see if we are testing or not
-const EMAIL_SERVICE_ACTIVE = process.env.EMAIL_SERVICE_STATUS === 'active' || false
+const EMAIL_SERVICE_ACTIVE =
+  process.env.EMAIL_SERVICE_STATUS === "active" || false;
+const IS_TESTING = process.env.NODE_ENV === "test";
+const EMAIL_FROM_ADDRESS = process.env.EMAIL_FROM_ADDRESS;
 
 function getDatabaseURI() {
-    const dbUser = process.env.DATABASE_USER || 'postgres'
-    const dbPass = process.env.DATABASE_PASS ? encodeURI(process.env.DATABASE_PASS) : 'postgres'
-    const dbHost = process.env.DATABASE_HOST || 'localhost'
-    const dbPort = process.env.DATABASE_PORT || 5432
-    const dbName = process.env.DATABASE_NAME || 'habit_tracker'
+  const dbUser = process.env.DATABASE_USER || "postgres";
+  const dbPass = process.env.DATABASE_PASS
+    ? encodeURI(process.env.DATABASE_PASS)
+    : "postgres";
+  const dbHost = process.env.DATABASE_HOST || "localhost";
+  const dbPort = process.env.DATABASE_PORT || 5432;
+  const dbName = process.env.DATABASE_NAME || "habit_tracker";
 
-    return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  return (
+    process.env.DATABASE_URL ||
+    `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  );
 }
 
-
-
-console.log("HabitTracker Config:".red)
-console.log("PORT:".blue, PORT)
-console.log("SECRET_KEY:".blue, SECRET_KEY)
-console.log("Database URI:".blue, getDatabaseURI())
-console.log("SENDGRID_API_key:".blue, SENDGRID_API_KEY)
-console.log("EMAIL_SERVICE_ACTIVE:".blue, EMAIL_SERVICE_ACTIVE)
-console.log('---')
-
+console.log(`${APPLICATION_NAME} Config:`.red);
+console.log("PORT:".blue, PORT);
+console.log("SECRET_KEY:".blue, SECRET_KEY);
+console.log("Database URI:".blue, getDatabaseURI());
+console.log("SENDGRID_API_key:".blue, SENDGRID_API_KEY);
+console.log("EMAIL_SERVICE_ACTIVE:".blue, EMAIL_SERVICE_ACTIVE);
+console.log("CLIENT_URL:".blue, CLIENT_URL);
+console.log("IS_TESTING:".blue, IS_TESTING);
+console.log("EMAIL_FROM_ADDRESS:".blue, EMAIL_FROM_ADDRESS);
+console.log("---");
 
 module.exports = {
-    PORT,
-    SECRET_KEY,
-    getDatabaseURI,
-    SENDGRID_API_KEY,
-    EMAIL_SERVICE_ACTIVE
-}
+  PORT,
+  SECRET_KEY,
+  getDatabaseURI,
+  SENDGRID_API_KEY,
+  EMAIL_SERVICE_ACTIVE,
+  IS_TESTING,
+  EMAIL_FROM_ADDRESS,
+  CLIENT_URL,
+  APPLICATION_NAME
+
+};
--- habit-tracker-express-api/services/email.js
+++ habit-tracker-express-api/services/email.js
@@ -5,7 +5,8 @@ const nodemailerSendgrid = require("nodemailer-sendgrid");
 class EmailService {
   constructor(config) {
     //initialize
-    const { isActive, apiKey } = config;
+    const { isActive, apiKey, emailFromAddress, clientUrl, applicationName } =
+      config;
 
     const transport = nodemailer.createTransport(
       nodemailerSendgrid({ apiKey })
@@ -13,17 +14,25 @@ class EmailService {
 
     this.transport = transport;
     this.isActive = isActive;
+    this.clientUrl = clientUrl;
+    this.emailFromAddress = emailFromAddress;
+    this.applicationName = applicationName;
   }
 
   async sendEmail(email) {
     // Make a case when not active
     if (this.isActive) {
-        if(!email.to) return { status: 400, email, error: [{ field: `to`, message: "Missing to field." }] };
+      if (!email.to)
+        return {
+          status: 400,
+          email,
+          error: [{ field: `to`, message: "Missing to field." }],
+        };
 
-        console.log(`Sending email to ${email.to} from ${email.from}`);
-        return { status: 202, email, error: null };
+      console.log(`Sending email to ${email.to} from ${email.from}`);
+      return { status: 202, email, error: null };
 
-        console.log("Email sent");
+      console.log("Email sent");
     }
 
     try {
@@ -33,22 +42,64 @@ class EmailService {
 
       return { status, email, error: res?.[0]?.body };
     } catch (error) {
-        console.error(`Error sending email: ${error}`);
+      console.error(`Error sending email: ${error}`);
 
-        const errors =  error?.response?.body?.errors
+      const errors = error?.response?.body?.errors;
 
-        return { status: 400, email, error: errors || error};
+      return { status: 400, email, error: errors || error };
     }
+  }
+  constructPasswordResetUrl(token) {
+    return `${this.clientUrl}/reset-password?token=${token}`;
   }
 
   async sendPasswordResetEmail(user, token) {
+    const resetPasswordUrl = this.constructPasswordResetUrl(token);
+
     const email = {
-    }
+      to: user.email,
+      from: this.emailFromAddress,
+      subject: `${this.applicationName} Password Reset`,
+      html: `
+      <html>
+        <body>
+          <p>Hi ${user.firstName},</p>
+          <p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
+          <p>Please click on the following link, or paste this into your browser to complete the process:</p>
+            <a href="${resetPasswordUrl}">
+              Reset your password
+            </a>
+          <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
+          <p>Thanks,</p>
+          <p>The Habit Tracker Team</p>
+        </body>
+      </html>
+      `,
+    };
+    return await this.sendEmail(email);
   }
 
+
   async sendPasswordResetConfirmationEmail(user, token) {
+    const resetPasswordUrl = this.constructPasswordResetUrl(token);
+
     const email = {
-    }
+      to: user.email,
+      from: this.emailFromAddress,
+      subject: `${this.applicationName} Password Reset Success`,
+      html: `
+      <html>
+        <body>
+          <p>Hi ${user.firstName},</p>
+          <p>This is a confirmation of a successful password reset for your accorunt.</p>
+          <p>If you did not request this, contact Yaw, Aloye, or Abdul immediately.</p>
+          <p>Thanks,</p>
+          <p>The Habit Tracker Team</p>
+        </body>
+      </html>
+      `,
+    };
+    return await this.sendEmail(email);
   }
 }
 
