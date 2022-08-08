c9f75206d78d1062d0b72490e613d4ca2e1f0030
Abdul Rauf Abdul Karim <abdul.karim@abdulka-ltmat6w.internal.salesforce.com>
2022-08-07 19:36:16 -0700

    Log count is now persistent

--- habit-traker-ui/src/components/HabitPage/HabitPage.jsx
+++ habit-traker-ui/src/components/HabitPage/HabitPage.jsx
@@ -11,9 +11,10 @@ import HabitForm from "../HabitForm/HabitForm";
 
 export default function HabitPage() {
   const [habits, setHabits] = useState([]);
+  const [filteredHabits, setFilteredHabits] = useState([])
   const [videoModalOpen, setVideoModalOpen] = useState(false);
-  const [formModalOpen, setFormModalOpen] = useState(false)
-  const [errors, setErrors] = useState("")
+  const [formModalOpen, setFormModalOpen] = useState(false);
+  const [errors, setErrors] = useState("");
   const [form, setForm] = useState({
     habitName: "",
     startDate: "",
@@ -22,6 +23,15 @@ export default function HabitPage() {
     period: "Per Day",
   });
   const navigate = useNavigate();
+  const [duration, setDuration] = useState("All Habits")
+
+  const durationOptions = [
+    { key: 1, label: "Past Habits", value: "Past Habits" },
+    { key: 2, label: "Current Habits", value: "Current Habits" },
+    { key: 3, label: "Future Habits", value: "Future Habits" },
+    { key : 4, label : "All Habits", value : "All Habits"}
+    ]
+
 
   useEffect(() => {
     const getHabits = async () => {
@@ -31,13 +41,14 @@ export default function HabitPage() {
       }
       if (data?.habits) {
         setHabits(data.habits);
+        setFilteredHabits(data.habits)
       }
     };
     getHabits();
   }, []);
 
   const closeModal = () => {
-    setVideoModalOpen(false); 
+    setVideoModalOpen(false);
     setForm({
       habitName: "",
       startDate: "",
@@ -45,32 +56,110 @@ export default function HabitPage() {
       period: "Per Day",
     });
     window.location.reload();
-  }
+  };
+
+  const handleOnInputChange = (event) =>{
+    
+
+    setDuration(event.target.value)
+    // setHabits(habits.filter(habit => habit.end_date))
+  } 
+
+  useEffect(() => {
+
+    let today = new Date();
+
+    if (duration == "Past Habits"){
+      console.log("selected past habits")
+      setFilteredHabits(habits.filter(habit => new Date(habit.end_date).getTime() < today.getTime()))
+    }
+    if (duration == "Current Habits"){
+      console.log("clicked current habits")
+      setFilteredHabits(habits.filter(habit => today.getTime() >= new Date(habit.start_date).getTime() && today.getTime() < new Date(habit.end_date).getTime()))
+    }
+    if (duration == "Future Habits"){
+      setFilteredHabits(habits.filter(habit => new Date(habit.start_date).getTime() > today.getTime()))
+    }
+    if (duration == "All Habits"){
+      setFilteredHabits(habits)
+    }
+
+  }, [duration])
 
   return (
-
-
-    
     <div className="habit-page">
       <Header />
       <main className="flex-grow">
         <section className="bg-gradient-to-b from-gray-100 to-white">
           <div className="max-w-6xl mx-auto px-4 sm:px-6">
             <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+            <div className="flex flex-wrap -mx-3 mb-4">
+                  <div className="w-full px-3">
+                    <label
+                      className="block text-gray-800 text-sm font-medium mb-1"
+                      htmlFor="duration"
+                    >
+                      Duration
+                    </label>
+                    <select
+                      name="duration"
+                      type="text"
+                      value={duration}
+                      className="form-input w-full text-gray-800"
+                      onChange={handleOnInputChange}
+                      required
+                    >
+                      {durationOptions.map((duration) => (
+                        <option key={duration.key} value={duration.label}>
+                          {duration.label}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
               <div className="hp-top">
+                
                 <h1 className="hp-title">All Habits</h1>
-                <div id="hp-create-btn-wrapper" className="btn-sm text-white bg-blue-600 hover:bg-blue-700 ml-3" style={{marginRight:"20px", marginBottom:"0.25rem"}}>
-                  <span onClick={(e) => { e.preventDefault(); e.stopPropagation(); setVideoModalOpen(true); }} aria-controls="modal">Create Habit</span>
+                <div
+                  id="hp-create-btn-wrapper"
+                  className="btn-sm text-white bg-blue-600 hover:bg-blue-700 ml-3"
+                  style={{ marginRight: "20px", marginBottom: "0.25rem" }}
+                >
+                  <span
+                    onClick={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                      setVideoModalOpen(true);
+                    }}
+                    aria-controls="modal"
+                  >
+                    Create Habit
+                  </span>
                 </div>
               </div>
-              <HabitGrid setFormModalOpen={setFormModalOpen} habits={habits} errors = {errors} setErrors = {setErrors} /> 
+              <HabitGrid
+                setFormModalOpen={setFormModalOpen}
+                habits={filteredHabits}
+                errors={errors}
+                setErrors={setErrors}
+              />
             </div>
-          </div>``
+          </div>
+          ``
           {/* Modal */}
-          <Modal id="create-habit-modal" ariaLabel="modal-headline" show={videoModalOpen} handleClose={closeModal}>
+          <Modal
+            id="create-habit-modal"
+            ariaLabel="modal-headline"
+            show={videoModalOpen}
+            handleClose={closeModal}
+          >
             <div className="relative pb-9/16">
               <div className="create-habit">
-                <HabitForm form={form} setForm={setForm} handleClose={closeModal}/>
+                <HabitForm
+                  form={form}
+                  setForm={setForm}
+                  handleClose={closeModal}
+                />
               </div>
             </div>
           </Modal>
@@ -102,7 +191,7 @@ function HabitCard({ habit, formModalOpen, setFormModalOpen, handleClose }) {
 
    const [tab, setTab] = useState(1);
   console.log("habit in habit card", habit)
-  let start_date = habit.temp_start_date
+  let start_date = new Date(habit.temp_start_date)
   let end_date = new Date(start_date)
   console.log("start_date", start_date)
   console.log("end date", end_date)
@@ -112,9 +201,9 @@ function HabitCard({ habit, formModalOpen, setFormModalOpen, handleClose }) {
   let today = new Date()
   today.setHours(0,0,0,0)
   end_date.setHours(0,0,0,0)
-  today.setDate(today.getDate())
+  today.setDate(today.getDate() + 1000)
 
-  const setPeriodEndDate = (date, period) => {
+  const getEndDate = (date, period) => {
 
     if (period == "Per Day") {
 
@@ -131,14 +220,38 @@ function HabitCard({ habit, formModalOpen, setFormModalOpen, handleClose }) {
 
 
   }
+  const setPeriodEndDate = (start, date, period) => {
+    console.log("start", start)
+    console.log("end", date)
+    if (period == "Per Day") {
+      date.setDate(date.getDate() + 1);
+    }
 
-  setPeriodEndDate(end_date, habit.period)
+    if (period == "Per Week") {
+      let daysAfterEndDate = today.getTime() - date.getTime()
+      daysAfterEndDate = (daysAfterEndDate)/ (1000 * 3600 * 24)
+      console.log("days after end date", daysAfterEndDate)
+      let daysAfterNextStartDate = daysAfterEndDate - (daysAfterEndDate % 7)
+      console.log("days after next start date", daysAfterNextStartDate)
+      start.setDate((new Date(date).getDate()) + daysAfterNextStartDate)
+      date.setDate(date.getDate() + (7 + daysAfterNextStartDate));
+      console.log("start date after functtion call", start)
+      console.log("end date after functtion call", date)
+
+    }
+
+    if (period == "Per Month") {
+
+    }
+
+    
+  };
+
+  getEndDate(end_date, habit.period)
   const updateLog = async (event) => {
     event.preventDefault();
-    
-
     if ( today.getTime() >= end_date.getTime()){
-      const tempObj = {tempStartDate : today}
+      
       const obj = {
         id : habit.id,
         habitName : habit.habit_name,
@@ -147,10 +260,9 @@ function HabitCard({ habit, formModalOpen, setFormModalOpen, handleClose }) {
         startDate : habit.start_date,
         endDate : habit.end_date
       }
+      setPeriodEndDate(start_date, end_date, habit.period)
+      const tempObj = {tempStartDate : start_date}
       const {data, error} = await apiClient.editHabit({...obj, ...tempObj})
-      end_date = today
-      setPeriodEndDate(end_date, habit.period)
-      start_date = today
 
     }
     const anotherDay = new Date(end_date).toISOString()
