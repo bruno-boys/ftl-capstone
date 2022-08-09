56a66ee7a03fd69b3cff05a655fdea3e6fc1eafc
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-08-09 14:25:25 -0700

    Set up backend for service workers and push notifications
    installed web-push and body-parser; created .env variables for the vapid keys; set up server.js

--- habit-traker-express-api/server.js
+++ habit-traker-express-api/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const webpush = require('web-push');
+const bodyParser = require('body-parser')
+const path = require('path')
 const { NotFoundError } = require('./utils/error');
-const { PORT } = require('./config')
+const { PORT, PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY } = require('./config')
 const { extractUserFromJwt } = require('./middleware/security')
 const authRoutes = require('./routes/auth')
 const habitRoutes = require('./routes/habits')
@@ -11,9 +14,15 @@ const buddyRoutes = require('./routes/buddy')
 
 const app = express();
 
+
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json({limit : '25mb'}));
+// set static path
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.json())
+
+webpush.setVapidDetails("mailto:habit.traker.customer.services@gmail.com", PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY);
 
 app.use(extractUserFromJwt)
 
@@ -21,6 +30,23 @@ app.use('/auth',authRoutes)
 app.use('/habits', habitRoutes)
 app.use('/stats', statisticRoutes);
 app.use('/buddy', buddyRoutes)
+
+
+//Subscribe Route
+app.post("/subscribe", (req,res) => {
+  // get pushSubscription object
+  const subscription = req.body;
+
+  // send 201 - resource created
+  res.status(201).json({});
+
+  // create payload
+  const payload = JSON.stringify({ title: 'HabitTraker Push Test' });
+
+  // pass the object into the sendNotification function
+  webpush.sendNotification(subscription, payload).catch(err => console.error(err));
+});
+
 
 
 app.get('/', (req,res) => {
