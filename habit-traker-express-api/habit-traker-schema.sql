1acf39a3cc25dbf446dd1af1dc4aaac82e8df062
Yaw Kessey-Ankomah <yawkesseyankomah@gmail.com>
2022-08-09 21:34:14 -0700

    Created table to keep of habits with reminders

--- habit-traker-express-api/habit-traker-schema.sql
+++ habit-traker-express-api/habit-traker-schema.sql
@@ -23,6 +23,12 @@ CREATE TABLE habits (
     end_date        TIMESTAMP
 );
 
+CREATE TABLE reminders (
+    id              SERIAL PRIMARY KEY, 
+    habit_id        INTEGER NOT NULL REFERENCES habits (id) ON DELETE CASCADE, 
+    time            TEXT NOT NULL 
+);
+
 CREATE TABLE tracked_habits (
     id              SERIAL PRIMARY KEY,
     habit_id        INTEGER NOT NULL REFERENCES habits (id) ON DELETE CASCADE,
