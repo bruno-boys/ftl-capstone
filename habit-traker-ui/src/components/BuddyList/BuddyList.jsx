8fdc991447e2f7ba20d69be34f5dc1959fab5d62
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-08-16 13:40:32 -0700

    created BuddyList component

---
+++ habit-traker-ui/src/components/BuddyList/BuddyList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import apiClient from "../../services/apiClient";
+import "./BuddyList.css"
+
+export default function BuddyGrid({ buddies, handleClose }) {
+    return (
+        <div className="buddy">
+            Hey Buddy!
+        </div>
+    )
+}
+
