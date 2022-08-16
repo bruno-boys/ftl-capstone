f5cf059b4406924b83d2d6f28c4cd43b2a34d76d
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-08-16 16:33:27 -0700

    made changes

--- habit-traker-ui/src/components/HabitPage/HabitPage.jsx
+++ habit-traker-ui/src/components/HabitPage/HabitPage.jsx
@@ -219,7 +219,7 @@ function HabitCard({ habit, formModalOpen, setFormModalOpen, handleClose }) {
 
   let today = new Date();
   today.setHours(0, 0, 0, 0);
-  today.setDate(today.getDate()+3);
+  today.setDate(today.getDate());
 
 
 
