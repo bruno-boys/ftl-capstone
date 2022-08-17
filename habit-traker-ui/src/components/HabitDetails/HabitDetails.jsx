655d0a29bf6a5c5f66f4e44c6c2adda8b9116bf3
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-08-17 00:34:28 -0700

    change css on habits-details

--- habit-traker-ui/src/components/HabitDetails/HabitDetails.jsx
+++ habit-traker-ui/src/components/HabitDetails/HabitDetails.jsx
@@ -5,7 +5,6 @@ import Header from '../../partials/Header';
 import Modal from '../../utils/Modal';
 import EditForm from '../EditForm/EditForm';
 import apiClient from '../../services/apiClient';
-import Calendar from 'react-calendar';
 import { useLocation } from 'react-router-dom'
 import './HabitDetails.css'
 import PieChart from './PieChart';
@@ -138,14 +137,16 @@ function HabitDetailContainer({missedHabits, completedHabits, habit, streakCount
 
     return (
         <div className="habit-detail-container">
+            <div className="subtitle">
+                <div className="periodLabel">{periodLabel}</div>
+                { localStorage.getItem("buddyView") == "false" ?
+                    <div className="habit-frequency">{habit.frequency} times {habit.period}</div>
+                    :
+                    <div className="habit-frequency">{buddyHabit?.frequency} times {buddyHabit?.period}</div>
+                }
+            </div>
             <div className="habit-details">
                 <div className="left">
-                    <div className="periodLabel">{periodLabel}</div>
-                    { localStorage.getItem("buddyView") == "false" ?
-                        <div className="habit-frequency">{habit.frequency} times {habit.period}</div>
-                        :
-                        <div className="habit-frequency">{buddyHabit?.frequency} times {buddyHabit?.period}</div>
-                    }
                     <div className="mb-8 md:mb-0">
                         <a
                         className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
@@ -163,36 +164,36 @@ function HabitDetailContainer({missedHabits, completedHabits, habit, streakCount
                             <a
                             className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
                             onClick={(e) => { e.preventDefault(); setTab(1); }}
-                            style={{width:"170px", display:"flex", justifyContent:"center"}}
+                            style={{width:"170px", display:"flex", flexDirection:"column", justifyContent:"center"}}
                             >
                                 <div>
                                     <div id="streak" className="font-bold leading-snug tracking-tight mb-1">Complete</div>
                                 </div>
-                                {completedHabits}
+                                <div>{completedHabits}</div>
                             </a>
 
                             <a
                             className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
                             onClick={(e) => { e.preventDefault(); setTab(1); }}
-                            style={{width:"170px", display:"flex", justifyContent:"center"}}
+                            style={{width:"170px", display:"flex", flexDirection:"column", justifyContent:"center"}}
                             >
                                 <div>
                                     <div id="streak" className="font-bold leading-snug tracking-tight mb-1">Missed</div>
                                 </div>
-                                {missedHabits}
+                                <div>{missedHabits}</div>
                             </a>
 
                             <a
                             className={`flex items-center text-lg p-5 rounded border transition duration-300 ease-in-out mb-3 ${tab !== 1 ? 'bg-white shadow-md border-gray-200 hover:shadow-lg' : 'bg-gray-200 border-transparent'}`}
                             onClick={(e) => { e.preventDefault(); setTab(1); }}
-                            style={{width:"170px", display:"flex", justifyContent:"center"}}
+                            style={{width:"170px", display:"flex", flexDirection:"column", justifyContent:"center"}}
                             >
                                 <div>
                                     <div id="streak" className="font-bold leading-snug tracking-tight mb-1">Total</div>
                                 </div>
+                                <div>{completedHabits + missedHabits}</div>
                             </a>
                         </div>
-                        <Calendar />
                     </div>
                 </div>
                 <div className="right">
